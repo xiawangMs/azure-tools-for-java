8cd23ff7d6e0933b450c95eef8fd01f28eb9c802
yuwe <yuwe@microsoft.com>
2022-08-09 15:49:57 +0800

    add sftp action handler. when invoke sftp action, open web deployment plugin for this machine

--- PluginsAndFeatures/azure-toolkit-for-intellij/azure-intellij-plugin-vm/src/main/java/com/microsoft/azure/toolkit/intellij/vm/ssh/sftpRemoteHostAction.java
+++ PluginsAndFeatures/azure-toolkit-for-intellij/azure-intellij-plugin-vm/src/main/java/com/microsoft/azure/toolkit/intellij/vm/ssh/sftpRemoteHostAction.java
@@ -1,3 +1,8 @@
+/*
+ * Copyright (c) Microsoft Corporation. All rights reserved.
+ * Licensed under the MIT License. See License.txt in the project root for license information.
+ */
+
 package com.microsoft.azure.toolkit.intellij.vm.ssh;
 
 import com.intellij.openapi.project.Project;
@@ -5,14 +10,19 @@ import com.intellij.openapi.project.Project;
 import com.intellij.openapi.wm.ToolWindow;
 import com.intellij.ssh.config.unified.SshConfig;
 import com.intellij.ssh.config.unified.SshConfigManager;
+import com.intellij.ui.content.Content;
 import com.jetbrains.plugins.webDeployment.config.AccessType;
 import com.jetbrains.plugins.webDeployment.config.GroupedServersConfigManagerImpl;
 import com.jetbrains.plugins.webDeployment.config.WebServerConfig;
 import com.jetbrains.plugins.webDeployment.ui.WebServerToolWindowFactory;
+import com.jetbrains.plugins.webDeployment.ui.WebServerToolWindowPanel;
+import com.microsoft.azure.toolkit.lib.common.messager.AzureMessager;
 import com.microsoft.azure.toolkit.lib.common.task.AzureTaskManager;
 import com.microsoft.azure.toolkit.lib.compute.virtualmachine.VirtualMachine;
+import org.apache.commons.lang3.reflect.MethodUtils;
 
 import javax.annotation.Nonnull;
+import java.lang.reflect.InvocationTargetException;
 import java.util.List;
 import java.util.Objects;
 
@@ -20,27 +30,40 @@ import java.util.Objects;
 public class sftpRemoteHostAction {
     public static void browseRemoteHost(VirtualMachine vm, @Nonnull Project project) {
         String sshConfigKey = String.format("Azure: %s", vm.name());
-        SshConfig curConfig = SshConfigManager.getInstance(project).findConfigByName(sshConfigKey);
+        SshConfig curSshConfig = SshConfigManager.getInstance(project).findConfigByName(sshConfigKey);
         GroupedServersConfigManagerImpl manager = new GroupedServersConfigManagerImpl(project);
 
         // get webserver config of current machine
-        WebServerConfig server = getWebServerConfigBySsh(curConfig, manager, project);
+        WebServerConfig server = getWebServerConfigBySsh(curSshConfig, manager, project);
         if (Objects.isNull(server)) {
             // create a new webServer config for current machine
-            server = new WebServerConfig(WebServerConfig.getNextId());
-            server.initializeNewCreatedServer(false);
-            server.setName(vm.getName());
-            server.getFileTransferConfig().setAccessType(AccessType.SFTP);
-            server.getFileTransferConfig().setPort(AccessType.SFTP.getDefaultPort());
-            server.getFileTransferConfig().setSshConfig(SshConfigManager.getInstance(project).findConfigByName(sshConfigKey));
+            server = createWebServerConfigBySsh(vm.getName(), curSshConfig);
             manager.addServer(server);
         }
 
         // open remote host plugin tool windows
+        WebServerConfig finalServer = server;
         AzureTaskManager.getInstance().runLater(() -> {
             ToolWindow toolWindow = WebServerToolWindowFactory.getWebServerToolWindow(project);
-            toolWindow.show((Runnable)null);
-            toolWindow.activate((Runnable)null);
+            // select current machine's config
+            Content[] contentList = toolWindow.getContentManager().getContents();
+            for (int i=0; i<contentList.length; i++) {
+                if (contentList[i].getComponent() instanceof WebServerToolWindowPanel) {
+                    try {
+                        WebServerToolWindowPanel panel =  ((WebServerToolWindowPanel) contentList[i].getComponent());
+                        panel.selectInServerByName(project, finalServer.getName(), finalServer.getRootPath());
+                        MethodUtils.invokeMethod(panel, true, "constructTree");
+                    } catch (NoSuchMethodException | IllegalAccessException |
+                             InvocationTargetException e) {
+                        AzureMessager.getMessager().error(e);
+                    }
+                    break;
+                }
+            }
+            if (!toolWindow.isActive()) {
+                toolWindow.show((Runnable)null);
+                toolWindow.activate((Runnable)null);
+            }
         });
     }
 
@@ -52,6 +75,16 @@ public class sftpRemoteHostAction {
             }
         }
         return null;
+    }
+
+    private static WebServerConfig createWebServerConfigBySsh(String serverName, SshConfig ssh) {
+        WebServerConfig serverConfig = new WebServerConfig(WebServerConfig.getNextId());
+        serverConfig.initializeNewCreatedServer(false);
+        serverConfig.setName(serverName);
+        serverConfig.getFileTransferConfig().setAccessType(AccessType.SFTP);
+        serverConfig.getFileTransferConfig().setPort(AccessType.SFTP.getDefaultPort());
+        serverConfig.getFileTransferConfig().setSshConfig(ssh);
+        return serverConfig;
     }
 
 }
