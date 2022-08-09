74898cce836eccf4a656f9c13cd7274fa932aac2
yuwe <yuwe@microsoft.com>
2022-08-09 14:04:11 +0800

    add sftp action handler. when invoke sftp action, add a new webserver config and open plugin

--- PluginsAndFeatures/azure-toolkit-for-intellij/azure-intellij-plugin-vm/src/main/java/com/microsoft/azure/toolkit/intellij/vm/ssh/sftpRemoteHostAction.java
+++ PluginsAndFeatures/azure-toolkit-for-intellij/azure-intellij-plugin-vm/src/main/java/com/microsoft/azure/toolkit/intellij/vm/ssh/sftpRemoteHostAction.java
@@ -3,33 +3,55 @@ package com.microsoft.azure.toolkit.intellij.vm.ssh;
 import com.intellij.openapi.project.Project;
 
 import com.intellij.openapi.wm.ToolWindow;
+import com.intellij.ssh.config.unified.SshConfig;
+import com.intellij.ssh.config.unified.SshConfigManager;
+import com.jetbrains.plugins.webDeployment.config.AccessType;
+import com.jetbrains.plugins.webDeployment.config.GroupedServersConfigManagerImpl;
+import com.jetbrains.plugins.webDeployment.config.WebServerConfig;
 import com.jetbrains.plugins.webDeployment.ui.WebServerToolWindowFactory;
 import com.microsoft.azure.toolkit.lib.common.task.AzureTaskManager;
 import com.microsoft.azure.toolkit.lib.compute.virtualmachine.VirtualMachine;
 
 import javax.annotation.Nonnull;
+import java.util.List;
+import java.util.Objects;
 
 
 public class sftpRemoteHostAction {
     public static void browseRemoteHost(VirtualMachine vm, @Nonnull Project project) {
+        String sshConfigKey = String.format("Azure: %s", vm.name());
+        SshConfig curConfig = SshConfigManager.getInstance(project).findConfigByName(sshConfigKey);
+        GroupedServersConfigManagerImpl manager = new GroupedServersConfigManagerImpl(project);
+
+        // get webserver config of current machine
+        WebServerConfig server = getWebServerConfigBySsh(curConfig, manager, project);
+        if (Objects.isNull(server)) {
+            // create a new webServer config for current machine
+            server = new WebServerConfig(WebServerConfig.getNextId());
+            server.initializeNewCreatedServer(false);
+            server.setName(vm.getName());
+            server.getFileTransferConfig().setAccessType(AccessType.SFTP);
+            server.getFileTransferConfig().setPort(AccessType.SFTP.getDefaultPort());
+            server.getFileTransferConfig().setSshConfig(SshConfigManager.getInstance(project).findConfigByName(sshConfigKey));
+            manager.addServer(server);
+        }
+
         // open remote host plugin tool windows
         AzureTaskManager.getInstance().runLater(() -> {
             ToolWindow toolWindow = WebServerToolWindowFactory.getWebServerToolWindow(project);
             toolWindow.show((Runnable)null);
             toolWindow.activate((Runnable)null);
         });
+    }
 
-        // open deployment configurable window
-//        AzureTaskManager.getInstance().runLater(() -> {
-//            DeploymentConfigurable configurable = new DeploymentConfigurable(project);
-//            ShowSettingsUtil.getInstance().editConfigurable(project, configurable, () -> {
-//                WebServerConfig server = new WebServerConfig();
-//                SshUiData data = server == null ? null : server.getOrCreateSshUiData(project);
-//
-//                String nameToSelect = server == null ? (project == null ? null : PublishConfig.getInstance(project).getDefaultServerOrGroupName()) : server.getName();
-//                System.out.println("wy hello");
-//            });
-//        });
+    private static WebServerConfig getWebServerConfigBySsh(SshConfig ssh, GroupedServersConfigManagerImpl manager, @Nonnull Project project) {
+        List<WebServerConfig> webServerConfigList = manager.getFlattenedServers();
+        for (int i=0; i<webServerConfigList.size(); i++) {
+            if (webServerConfigList.get(i).findSshConfig(project).equals(ssh)) {
+                return webServerConfigList.get(i);
+            }
+        }
+        return null;
     }
 
 }
