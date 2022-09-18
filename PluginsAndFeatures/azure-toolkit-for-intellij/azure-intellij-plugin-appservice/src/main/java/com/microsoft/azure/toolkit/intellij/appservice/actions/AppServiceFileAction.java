583f68c7bb1736a21740ab0451bd86c261acecc6
yuwe <yuwe@microsoft.com>
2022-09-18 16:18:29 +0800

    #1972398 update message

--- PluginsAndFeatures/azure-toolkit-for-intellij/azure-intellij-plugin-appservice/src/main/java/com/microsoft/azure/toolkit/intellij/appservice/actions/AppServiceFileAction.java
+++ PluginsAndFeatures/azure-toolkit-for-intellij/azure-intellij-plugin-appservice/src/main/java/com/microsoft/azure/toolkit/intellij/appservice/actions/AppServiceFileAction.java
@@ -185,7 +185,8 @@ public class AppServiceFileAction {
     @SneakyThrows
     public void saveAppServiceFile(@NotNull AppServiceFile file, @Nullable Project project, @Nullable File dest) {
         if (!file.getApp().getFormalStatus().isRunning()) {
-            AzureMessager.getMessager().warning(AzureString.format("Can not download files for app service with status %s", file.getApp().getStatus()));
+            AzureMessager.getMessager().warning(AzureString.format(
+                    "Can not download files from app service(%s) because it's not running", file.getApp().getName()));
             return;
         }
         final Action<Void> retry = Action.retryFromFailure((() -> this.saveAppServiceFile(file, project, dest)));
