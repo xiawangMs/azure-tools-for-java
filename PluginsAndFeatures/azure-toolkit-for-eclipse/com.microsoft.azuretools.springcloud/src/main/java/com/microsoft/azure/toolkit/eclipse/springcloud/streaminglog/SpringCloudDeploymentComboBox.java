f07b4e073fd893d0002f3ca85d9c7ba256d62358
Mingliang Wang <wangmi@microsoft.com>
2022-10-21 09:33:35 +0800

    fix eclipse compilation error.

--- PluginsAndFeatures/azure-toolkit-for-eclipse/com.microsoft.azuretools.springcloud/src/main/java/com/microsoft/azure/toolkit/eclipse/springcloud/streaminglog/SpringCloudDeploymentComboBox.java
+++ PluginsAndFeatures/azure-toolkit-for-eclipse/com.microsoft.azuretools.springcloud/src/main/java/com/microsoft/azure/toolkit/eclipse/springcloud/streaminglog/SpringCloudDeploymentComboBox.java
@@ -14,10 +14,9 @@ import org.eclipse.swt.widgets.Composite;
 
 import com.microsoft.azure.toolkit.eclipse.common.component.AzureComboBox;
 import com.microsoft.azure.toolkit.lib.springcloud.SpringCloudApp;
-import com.microsoft.azure.toolkit.lib.springcloud.SpringCloudDeploymentInstanceEntity;
-import com.azure.resourcemanager.appplatform.models.DeploymentInstance;
+import com.microsoft.azure.toolkit.lib.springcloud.SpringCloudAppInstance;
 
-public class SpringCloudDeploymentComboBox extends AzureComboBox<DeploymentInstance> {
+public class SpringCloudDeploymentComboBox extends AzureComboBox<SpringCloudAppInstance> {
 
     private SpringCloudApp app;
 
@@ -38,15 +37,15 @@ public class SpringCloudDeploymentComboBox extends AzureComboBox<DeploymentInsta
     }
 
     @Override
-    protected List<? extends DeploymentInstance> loadItems() {
+    protected List<? extends SpringCloudAppInstance> loadItems() {
         return Optional.ofNullable(app).map(SpringCloudApp::getActiveDeployment)
                 .map(deployment -> deployment.getInstances()).orElse(Collections.emptyList());
     }
 
     @Override
     protected String getItemText(Object item) {
-        return item instanceof DeploymentInstance
-                ? ((DeploymentInstance) item).name()
+        return item instanceof SpringCloudAppInstance
+                ? ((SpringCloudAppInstance) item).getName()
                 : super.getItemText(item);
     }
 
