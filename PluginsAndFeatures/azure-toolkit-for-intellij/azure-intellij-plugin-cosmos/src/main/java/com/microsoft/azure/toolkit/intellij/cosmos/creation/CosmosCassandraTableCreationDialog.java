0329f2f76dc01ba497b90abe825cfe90c8f250c7
Hanxiao Liu <hanli@microsoft.com>
2022-10-30 17:22:46 +0800

    Resolve validation issue for cosmos container creation

--- PluginsAndFeatures/azure-toolkit-for-intellij/azure-intellij-plugin-cosmos/src/main/java/com/microsoft/azure/toolkit/intellij/cosmos/creation/CosmosCassandraTableCreationDialog.java
+++ PluginsAndFeatures/azure-toolkit-for-intellij/azure-intellij-plugin-cosmos/src/main/java/com/microsoft/azure/toolkit/intellij/cosmos/creation/CosmosCassandraTableCreationDialog.java
@@ -5,7 +5,9 @@
 package com.microsoft.azure.toolkit.intellij.cosmos.creation;
 
 import com.intellij.openapi.project.Project;
+import com.intellij.openapi.ui.ValidationInfo;
 import com.intellij.ui.HyperlinkLabel;
+import com.intellij.util.ui.JBFont;
 import com.microsoft.azure.toolkit.intellij.common.AzureDialog;
 import com.microsoft.azure.toolkit.intellij.common.AzureTextInput;
 import com.microsoft.azure.toolkit.lib.common.form.AzureForm;
@@ -45,7 +47,12 @@ public class CosmosCassandraTableCreationDialog extends AzureDialog<CassandraTab
         super.init();
         txtTableId.setRequired(true);
         txtTableId.addValidator(this::validateTableId);
-        txtTableId.addValueChangedListener(value -> lblCreateCommand.setText(String.format(TEMPLATE, keyspace.getName(), value)));
+        txtTableId.addValueChangedListener(value -> {
+            final String command = String.format(TEMPLATE, keyspace.getName(), value);
+            final String labelValue = command.length() > 50 ? command.substring(0, 47) + "..." : command;
+            lblCreateCommand.setText(labelValue);
+            lblCreateCommand.setToolTipText(command);
+        });
 
         lblTableId.setLabelFor(txtTableId);
         lblCommand.setLabelFor(textSchema);
@@ -53,11 +60,15 @@ public class CosmosCassandraTableCreationDialog extends AzureDialog<CassandraTab
         lblLearnMore.setHyperlinkText("Learn More");
         lblLearnMore.setHyperlinkTarget("https://aka.ms/cassandra-create-table");
 
+        textSchema.setFont(JBFont.label());
         this.pack();
     }
 
     private AzureValidationInfo validateTableId() {
         final String value = txtTableId.getValue();
+        if (StringUtils.isNotEmpty(value) && value.length() > 48) {
+            return AzureValidationInfo.error("Table id should be less than 48 characters.", txtTableId);
+        }
         return StringUtils.endsWith(value, StringUtils.SPACE) || StringUtils.containsAny(value, "\\", "/", "#", "?", "%", "-") ?
                 AzureValidationInfo.error("Table Id should not end with space nor contain characters '\\', '/', '#', '?', '%', '-'", txtTableId) : AzureValidationInfo.success(txtTableId);
     }
@@ -89,6 +100,18 @@ public class CosmosCassandraTableCreationDialog extends AzureDialog<CassandraTab
     public void setValue(CassandraTableDraft.CassandraTableConfig data) {
         txtTableId.setValue(data.getTableId());
         textSchema.setText(data.getSchema());
+    }
+
+    @Override
+    protected List<ValidationInfo> doValidateAll() {
+        final List<ValidationInfo> validationInfos = super.doValidateAll();
+        if (validationInfos.isEmpty()) {
+            final String schema = textSchema.getText();
+            if (StringUtils.isBlank(schema)) {
+                validationInfos.add(new ValidationInfo("Command is required", textSchema));
+            }
+        }
+        return validationInfos;
     }
 
     @Override
