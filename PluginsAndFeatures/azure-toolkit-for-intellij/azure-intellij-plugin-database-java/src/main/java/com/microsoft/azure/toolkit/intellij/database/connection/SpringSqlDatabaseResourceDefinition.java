558d6d6ec5d279ab77e4ccc03dcd1993bc368112
Hanxiao Liu <hanli@microsoft.com>
2022-11-28 16:09:01 +0800

    Add Java resource definition for mysql/sqlserver/postgre, AB#2007077

--- PluginsAndFeatures/azure-toolkit-for-intellij/azure-intellij-plugin-database-java/src/main/java/com/microsoft/azure/toolkit/intellij/database/connection/SpringSqlDatabaseResourceDefinition.java
+++ PluginsAndFeatures/azure-toolkit-for-intellij/azure-intellij-plugin-database-java/src/main/java/com/microsoft/azure/toolkit/intellij/database/connection/SpringSqlDatabaseResourceDefinition.java
@@ -3,25 +3,20 @@
  * Licensed under the MIT License. See License.txt in the project root for license information.
  */
 
-package com.microsoft.azure.intellij.database.connection;
+package com.microsoft.azure.toolkit.intellij.database.connection;
 
 import com.microsoft.azure.toolkit.intellij.connector.Connection;
 import com.microsoft.azure.toolkit.intellij.connector.spring.SpringSupported;
-import com.microsoft.azure.toolkit.intellij.database.connection.SqlDatabaseResourceDefinition;
 import com.microsoft.azure.toolkit.lib.database.entity.IDatabase;
 import org.apache.commons.lang3.tuple.Pair;
 
 import java.util.ArrayList;
 import java.util.List;
 
-public abstract class SpringSqlDatabaseResourceDefinition<T extends IDatabase> extends SqlDatabaseResourceDefinition<T> implements SpringSupported<T> {
-
-    protected SpringSqlDatabaseResourceDefinition(String name, String title, String icon) {
-        super(name, title, icon);
-    }
+public interface SpringSqlDatabaseResourceDefinition<T extends IDatabase> extends SpringSupported<T> {
 
     @Override
-    public List<Pair<String, String>> getSpringProperties() {
+    default public List<Pair<String, String>> getSpringProperties() {
         final List<Pair<String, String>> properties = new ArrayList<>();
         properties.add(Pair.of("spring.datasource.url", String.format("${%s_URL}", Connection.ENV_PREFIX)));
         properties.add(Pair.of("spring.datasource.username", String.format("${%s_USERNAME}", Connection.ENV_PREFIX)));
---
+++ PluginsAndFeatures/azure-toolkit-for-intellij/azure-intellij-plugin-database-java/src/main/java/com/microsoft/azure/toolkit/intellij/database/sqlserver/connection/JavaSqlServerDatabaseResourceDefinition.java
@@ -0,0 +1,12 @@
+/*
+ * Copyright (c) Microsoft Corporation. All rights reserved.
+ * Licensed under the MIT License. See License.txt in the project root for license information.
+ */
+
+package com.microsoft.azure.toolkit.intellij.database.sqlserver.connection;
+
+import com.microsoft.azure.toolkit.intellij.database.connection.SpringSqlDatabaseResourceDefinition;
+import com.microsoft.azure.toolkit.lib.sqlserver.MicrosoftSqlDatabase;
+
+public class JavaSqlServerDatabaseResourceDefinition extends SqlServerDatabaseResourceDefinition implements SpringSqlDatabaseResourceDefinition<MicrosoftSqlDatabase> {
+}
