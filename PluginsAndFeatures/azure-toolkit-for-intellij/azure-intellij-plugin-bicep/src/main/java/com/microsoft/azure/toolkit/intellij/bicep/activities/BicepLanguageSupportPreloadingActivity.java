adc6c7acabbabff4b96eb4ada5cc52cd588f5e62
Hanxiao Liu <hanli@microsoft.com>
2022-12-15 00:19:06 +0800

    Enable bicep language support in intellij toolkit

---
+++ PluginsAndFeatures/azure-toolkit-for-intellij/azure-intellij-plugin-bicep/src/main/java/com/microsoft/azure/toolkit/intellij/bicep/activities/BicepLanguageSupportPreloadingActivity.java
@@ -0,0 +1,50 @@
+/*
+ * Copyright (c) Microsoft Corporation. All rights reserved.
+ * Licensed under the MIT License. See License.txt in the project root for license information.
+ */
+
+package com.microsoft.azure.toolkit.intellij.bicep.activities;
+
+import com.intellij.openapi.application.PreloadingActivity;
+import com.intellij.openapi.progress.ProgressIndicator;
+import com.microsoft.azure.toolkit.lib.common.messager.AzureMessager;
+import org.apache.commons.io.FileUtils;
+import org.apache.commons.lang3.StringUtils;
+import org.jetbrains.annotations.NotNull;
+import org.wso2.lsp4intellij.IntellijLanguageClient;
+import org.wso2.lsp4intellij.client.languageserver.serverdefinition.RawCommandServerDefinition;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.Objects;
+
+import static com.microsoft.azure.toolkit.intellij.bicep.highlight.BicepEditorHighlighterProvider.LIB_ROOT;
+import static com.microsoft.azure.toolkit.lib.common.utils.CommandUtils.exec;
+
+public class BicepLanguageSupportPreloadingActivity extends PreloadingActivity {
+
+    public static final String BICEP_LANGSERVER = "bicep-langserver";
+    public static final String BICEP_LANG_SERVER_DLL = "Bicep.LangServer.dll";
+
+    @Override
+    public void preload(@NotNull ProgressIndicator indicator) {
+        final File bicep = FileUtils.getFile(LIB_ROOT, BICEP_LANGSERVER, BICEP_LANG_SERVER_DLL);
+        if (Objects.isNull(bicep) || !bicep.exists()) {
+            AzureMessager.getMessager().warning("Bicep Language Server was not found, disable related features.");
+            return;
+        }
+        if (!isDotnetRuntimeReady()) {
+            AzureMessager.getMessager().warning("Dotnet runtime was not founded, Bicep language support was disabled");
+            return;
+        }
+        IntellijLanguageClient.addServerDefinition(new RawCommandServerDefinition("bicep", new String[]{"dotnet", bicep.getAbsolutePath(), "--stdio"}));
+    }
+
+    private boolean isDotnetRuntimeReady() {
+        try {
+            return StringUtils.isNoneBlank(exec("dotnet --version"));
+        } catch (final IOException e) {
+            return false;
+        }
+    }
+}
---
+++ PluginsAndFeatures/azure-toolkit-for-intellij/azure-intellij-plugin-bicep/src/main/java/com/microsoft/azure/toolkit/intellij/bicep/activities/BicepStartupActivity.java
@@ -0,0 +1,30 @@
+/*
+ * Copyright (c) Microsoft Corporation. All rights reserved.
+ * Licensed under the MIT License. See License.txt in the project root for license information.
+ */
+
+package com.microsoft.azure.toolkit.intellij.bicep.activities;
+
+import com.intellij.openapi.diagnostic.Logger;
+import com.intellij.openapi.project.Project;
+import com.intellij.openapi.startup.StartupActivity;
+import com.microsoft.azure.toolkit.intellij.bicep.highlight.ZipResourceUtils;
+import com.microsoft.azure.toolkit.lib.common.messager.ExceptionNotification;
+import com.microsoft.azure.toolkit.lib.common.operation.AzureOperation;
+
+import javax.annotation.Nonnull;
+
+@SuppressWarnings("ResultOfMethodCallIgnored")
+public class BicepStartupActivity implements StartupActivity.DumbAware {
+    public static final String TEXTMATE_ZIP = "/textmate.zip";
+    public static final String BICEP_SERVER = "/bicep-langserver.zip";
+    protected static final Logger LOG = Logger.getInstance(BicepStartupActivity.class);
+
+    @Override
+    @ExceptionNotification
+    @AzureOperation(name = "platform/bicep.prepare_textmate_bundles")
+    public void runActivity(@Nonnull Project project) {
+        ZipResourceUtils.copyTextMateBundlesFromJar(TEXTMATE_ZIP, "textmate");
+        ZipResourceUtils.copyTextMateBundlesFromJar(BICEP_SERVER, "bicep-langserver");
+    }
+}
