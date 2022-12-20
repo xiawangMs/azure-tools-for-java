958bafb3d5957a5acd4125b55c036b8a31c71fa2
Mingliang Wang <wangmi@microsoft.com>
2022-12-20 15:28:52 +0800

    bicep textmate bundles are registered multiple times sometimes.

--- PluginsAndFeatures/azure-toolkit-for-intellij/azure-intellij-plugin-bicep/src/main/java/com/microsoft/azure/toolkit/intellij/bicep/highlight/BicepEditorHighlighterProvider.java
+++ PluginsAndFeatures/azure-toolkit-for-intellij/azure-intellij-plugin-bicep/src/main/java/com/microsoft/azure/toolkit/intellij/bicep/highlight/BicepEditorHighlighterProvider.java
@@ -11,8 +11,11 @@ import com.intellij.openapi.editor.highlighter.EditorHighlighter;
 import com.intellij.openapi.fileTypes.FileType;
 import com.intellij.openapi.project.Project;
 import com.intellij.openapi.vfs.VirtualFile;
+import com.microsoft.azure.toolkit.lib.common.exception.SystemException;
 import com.microsoft.azure.toolkit.lib.common.operation.AzureOperation;
 import com.microsoft.intellij.CommonConst;
+import org.apache.commons.lang3.StringUtils;
+import org.apache.commons.lang3.reflect.FieldUtils;
 import org.jetbrains.plugins.textmate.TextMateService;
 import org.jetbrains.plugins.textmate.configuration.BundleConfigBean;
 import org.jetbrains.plugins.textmate.configuration.TextMateSettings;
@@ -26,6 +29,7 @@ import java.nio.file.Path;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Objects;
+import java.util.concurrent.locks.Lock;
 
 public class BicepEditorHighlighterProvider extends TextMateEditorHighlighterProvider {
     public static final String LIB_ROOT = Path.of(PluginPathManager.getPluginHomePath(CommonConst.PLUGIN_NAME), "lib").toString();
@@ -34,20 +38,33 @@ public class BicepEditorHighlighterProvider extends TextMateEditorHighlighterPro
     @AzureOperation("platform/bicep.get_editor_highlighter")
     public EditorHighlighter getEditorHighlighter(@Nullable Project project, @Nonnull FileType fileType, @Nullable VirtualFile virtualFile, @Nonnull EditorColorsScheme colors) {
         final TextMateLanguageDescriptor descriptor = TextMateService.getInstance().getLanguageDescriptorByExtension("bicep");
-        final TextMateSettingsState state = TextMateSettings.getInstance().getState();
-        if (Objects.isNull(descriptor) && Objects.nonNull(state)) { // register textmate if not registered
-            registerBicepTextMateBundleAndReload(state);
+        if (Objects.isNull(descriptor)) { // register textmate if not registered
+            registerBicepTextMateBundleAndReload();
         }
         return super.getEditorHighlighter(project, fileType, virtualFile, colors);
     }
 
     @AzureOperation("boundary/bicep.register_textmate_bundles")
-    private static void registerBicepTextMateBundleAndReload(TextMateSettingsState state) {
-        final Collection<BundleConfigBean> bundles = state.getBundles();
-        final ArrayList<BundleConfigBean> newBundles = new ArrayList<>(bundles);
-        newBundles.add(new BundleConfigBean("bicep", Path.of(LIB_ROOT, "textmate", "bicep").toString(), true));
-        newBundles.add(new BundleConfigBean("bicepparam", Path.of(LIB_ROOT, "textmate", "bicepparam").toString(), true));
-        state.setBundles(newBundles);
-        TextMateService.getInstance().reloadEnabledBundles();
+    private static synchronized void registerBicepTextMateBundleAndReload() {
+        final TextMateSettingsState state = TextMateSettings.getInstance().getState();
+        try {
+            if (Objects.nonNull(state)) {
+                final Lock registrationLock = (Lock) FieldUtils.readField(TextMateService.getInstance(), "myRegistrationLock", true);
+                try {
+                    registrationLock.lock();
+                    final Collection<BundleConfigBean> bundles = state.getBundles();
+                    final ArrayList<BundleConfigBean> newBundles = new ArrayList<>(bundles);
+                    newBundles.removeIf(bundle -> StringUtils.equalsAnyIgnoreCase(bundle.getName(), "bicep", "bicepparam"));
+                    newBundles.add(new BundleConfigBean("bicep", Path.of(LIB_ROOT, "textmate", "bicep").toString(), true));
+                    newBundles.add(new BundleConfigBean("bicepparam", Path.of(LIB_ROOT, "textmate", "bicepparam").toString(), true));
+                    state.setBundles(newBundles);
+                    TextMateService.getInstance().reloadEnabledBundles();
+                } finally {
+                    registrationLock.unlock();
+                }
+            }
+        } catch (final IllegalAccessException e) {
+            throw new SystemException("can not acquire lock of 'TextMateService'.", e);
+        }
     }
 }
