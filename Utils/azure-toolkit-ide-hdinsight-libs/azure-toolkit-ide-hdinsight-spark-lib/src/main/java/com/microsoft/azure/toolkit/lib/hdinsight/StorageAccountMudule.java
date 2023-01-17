5621ac86cad4020c7c0ad6acd004d605049a08f1
fengshuaihu <fengshuaihu99@163.com>
2023-01-17 16:51:16 +0800

    fix storageaccount not support type err

--- Utils/azure-toolkit-ide-hdinsight-libs/azure-toolkit-ide-hdinsight-spark-lib/src/main/java/com/microsoft/azure/toolkit/lib/hdinsight/StorageAccountMudule.java
+++ Utils/azure-toolkit-ide-hdinsight-libs/azure-toolkit-ide-hdinsight-spark-lib/src/main/java/com/microsoft/azure/toolkit/lib/hdinsight/StorageAccountMudule.java
@@ -1,10 +1,14 @@
 package com.microsoft.azure.toolkit.lib.hdinsight;
 
+import com.azure.core.util.paging.ContinuablePage;
 import com.microsoft.azure.toolkit.lib.common.model.AbstractAzResourceModule;
+import com.microsoft.azure.toolkit.lib.common.model.page.ItemPage;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
 import javax.annotation.Nonnull;
+import java.util.Collections;
+import java.util.Iterator;
 import java.util.Optional;
 import java.util.stream.Stream;
 
@@ -26,13 +30,17 @@ public class StorageAccountMudule extends AbstractAzResourceModule <StorageAccou
         ).orElse(Stream.empty());
     }
 
+    @Nonnull
+    @Override
+    protected Iterator<? extends ContinuablePage<String, com.azure.resourcemanager.hdinsight.models.StorageAccount>> loadResourcePagesFromAzure() {
+        return Collections.singletonList(new ItemPage<>(this.loadResourcesFromAzure())).iterator();
+    }
+
     @NotNull
     @Override
     protected StorageAccountNode newResource(@NotNull com.azure.resourcemanager.hdinsight.models.StorageAccount storageAccount) {
         return new StorageAccountNode(NAME,this.sparkClusterNode.getResourceGroupName(),this);
     }
-
-
 
     @NotNull
     @Override
