f33835de6da303ad3ed9dda6167fca2ad60c9dd0
yuwe <yuwe@microsoft.com>
2023-02-01 14:14:17 +0800

    AB#2027473 open Azure Monitor with a null workspace when not log in. add @Nullable annotation to workspace

--- PluginsAndFeatures/azure-toolkit-for-intellij/azure-intellij-plugin-applicationinsights/src/main/java/com/microsoft/azure/toolkit/intellij/applicationinsights/task/OpenLogsInMonitorAction.java
+++ PluginsAndFeatures/azure-toolkit-for-intellij/azure-intellij-plugin-applicationinsights/src/main/java/com/microsoft/azure/toolkit/intellij/applicationinsights/task/OpenLogsInMonitorAction.java
@@ -33,8 +33,8 @@ public class OpenLogsInMonitorAction {
     }
 
     public void execute() {
-        Optional.ofNullable(getWorkspace()).ifPresent(it -> AzureTaskManager.getInstance().runLater(() ->
-                AzureMonitorManager.getInstance().openMonitorWindow(project, it, resourceId)));
+        AzureTaskManager.getInstance().runLater(() ->
+                AzureMonitorManager.getInstance().openMonitorWindow(project, getWorkspace(), resourceId));
     }
 
     @Nullable
