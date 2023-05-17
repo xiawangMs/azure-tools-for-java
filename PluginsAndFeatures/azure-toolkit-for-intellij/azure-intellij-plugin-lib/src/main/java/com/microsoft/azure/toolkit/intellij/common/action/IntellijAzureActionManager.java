e43e4e363835184266eb592595e27bc0662ae5cb
Flanker-MSFT <flanker-msft@KrishnaKT-PC.fareast.corp.microsoft.com>
2023-05-17 21:02:09 +0800

    Update visible logic for intellij azure action wrapper

--- PluginsAndFeatures/azure-toolkit-for-intellij/azure-intellij-plugin-lib/src/main/java/com/microsoft/azure/toolkit/intellij/common/action/IntellijAzureActionManager.java
+++ PluginsAndFeatures/azure-toolkit-for-intellij/azure-intellij-plugin-lib/src/main/java/com/microsoft/azure/toolkit/intellij/common/action/IntellijAzureActionManager.java
@@ -143,12 +143,13 @@ public class IntellijAzureActionManager extends AzureActionManager {
             final boolean visible;
             final boolean isAbstractAzResource = source instanceof AbstractAzResource;
 
-            if (isAbstractAzResource && "[LinkedCluster]".equals(((AbstractAzResource<?, ?, ?>) source).getSubscription().getId())) {
+            if (isAbstractAzResource && ((AbstractAzResource<?, ?, ?>) source).isEmulatorResource()) {
+                visible = view.isVisible();
+            } else if (isAbstractAzResource && "[LinkedCluster]".equals(((AbstractAzResource<?, ?, ?>) source).getSubscription().getId())) {
                 visible = true;
             } else {
                 final boolean isResourceInOtherSubs = isAbstractAzResource && !((AbstractAzResource<?, ?, ?>) source).getSubscription().isSelected() && this.action.isAuthRequired();
-                final boolean enableResourceInOtherSubs = source instanceof AbstractAzResource && ((AbstractAzResource<?, ?, ?>) source).isEmulatorResource();
-                visible = (!isResourceInOtherSubs || enableResourceInOtherSubs) && view.isVisible() && Objects.nonNull(action.getHandler(source, e));
+                visible = !isResourceInOtherSubs && view.isVisible() && Objects.nonNull(action.getHandler(source, e));
             }
 
             presentation.setVisible(visible);
