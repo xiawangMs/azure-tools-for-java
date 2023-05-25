d5d6498319b0bde0c60f65b0b2001afa3ff8a8af
Miller Wang <wangmi@microsoft.com>
2023-05-25 17:12:33 +0800

    load .env only for those have DotEnvBeforeRunTasks

--- PluginsAndFeatures/azure-toolkit-for-intellij/azure-intellij-resource-connector-lib-java/src/main/java/com/microsoft/azure/toolkit/intellij/connector/dotazure/DotEnvRunConfigurationExtension.java
+++ PluginsAndFeatures/azure-toolkit-for-intellij/azure-intellij-resource-connector-lib-java/src/main/java/com/microsoft/azure/toolkit/intellij/connector/dotazure/DotEnvRunConfigurationExtension.java
@@ -8,7 +8,6 @@ import com.microsoft.azure.toolkit.lib.common.messager.ExceptionNotification;
 import com.microsoft.azure.toolkit.lib.common.operation.AzureOperation;
 
 import javax.annotation.Nonnull;
-import java.util.List;
 
 public class DotEnvRunConfigurationExtension extends RunConfigurationExtension {
 
@@ -16,23 +15,15 @@ public class DotEnvRunConfigurationExtension extends RunConfigurationExtension {
     @ExceptionNotification
     @AzureOperation(name = "user/dotazure.load_env.config", params = {"config.getName()"})
     public <T extends RunConfigurationBase<?>> void updateJavaParameters(@Nonnull T config, @Nonnull JavaParameters params, RunnerSettings s) {
-        final List<DotEnvBeforeRunTaskProvider.LoadDotEnvBeforeRunTask> tasks = config.getBeforeRunTasks().stream()
+        config.getBeforeRunTasks().stream()
             .filter(t -> t instanceof DotEnvBeforeRunTaskProvider.LoadDotEnvBeforeRunTask)
-            .map(t -> (DotEnvBeforeRunTaskProvider.LoadDotEnvBeforeRunTask) t).toList();
-        if (tasks.isEmpty()) {
-            AzureModule.createIfSupport(config).filter(AzureModule::isInitialized)
-                .map(AzureModule::getDefaultProfile).stream()
-                .flatMap(e -> e.load().stream())
-                .forEach(p -> params.addEnv(p.getKey(), p.getValue()));
-        } else {
-            tasks.stream()
+            .map(t -> (DotEnvBeforeRunTaskProvider.LoadDotEnvBeforeRunTask) t)
                 .flatMap(t -> t.loadEnv().stream())
                 .forEach(p -> params.addEnv(p.getKey(), p.getValue()));
-        }
     }
 
     @Override
     public boolean isApplicableFor(@Nonnull RunConfigurationBase<?> configuration) {
-        return AzureModule.isSupported(configuration) || configuration.getBeforeRunTasks().stream().anyMatch(t -> t instanceof DotEnvBeforeRunTaskProvider.LoadDotEnvBeforeRunTask);
+        return configuration.getBeforeRunTasks().stream().anyMatch(t -> t instanceof DotEnvBeforeRunTaskProvider.LoadDotEnvBeforeRunTask);
     }
 }
