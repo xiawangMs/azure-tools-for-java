1be2d257f95eebf57b0955010ca7b681b1333bb6
Miller Wang <wangmi@microsoft.com>
2023-06-08 20:58:55 +0800

    add back missing file header.

--- PluginsAndFeatures/azure-toolkit-for-intellij/azure-intellij-plugin-facet/src/main/java/com/microsoft/azure/toolkit/intellij/facet/projectexplorer/IAzureFacetNode.java
+++ PluginsAndFeatures/azure-toolkit-for-intellij/azure-intellij-plugin-facet/src/main/java/com/microsoft/azure/toolkit/intellij/facet/projectexplorer/IAzureFacetNode.java
@@ -5,11 +5,6 @@
 
 package com.microsoft.azure.toolkit.intellij.facet.projectexplorer;
 
-/*
- * Copyright (c) Microsoft Corporation. All rights reserved.
- * Licensed under the MIT License. See License.txt in the project root for license information.
- */
-
 import com.intellij.openapi.actionSystem.DataProvider;
 import com.microsoft.azure.toolkit.lib.common.action.IActionGroup;
 
