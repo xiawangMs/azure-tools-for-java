a72db296dff4f3d0e7a9cca6dbb695bfb7f80420
Miller Wang <wangmi@microsoft.com>
2023-06-14 08:44:13 +0800

    move default `Connect Azure resources`(when there are no connections) to ConnectionsNode

--- PluginsAndFeatures/azure-toolkit-for-intellij/azure-intellij-plugin-facet/src/main/java/com/microsoft/azure/toolkit/intellij/facet/projectexplorer/ConnectionsNode.java
+++ PluginsAndFeatures/azure-toolkit-for-intellij/azure-intellij-plugin-facet/src/main/java/com/microsoft/azure/toolkit/intellij/facet/projectexplorer/ConnectionsNode.java
@@ -12,11 +12,14 @@ import com.microsoft.azure.toolkit.intellij.connector.ResourceConnectionActionsC
 import com.microsoft.azure.toolkit.intellij.connector.dotazure.AzureModule;
 import com.microsoft.azure.toolkit.lib.common.action.AzureActionManager;
 import com.microsoft.azure.toolkit.lib.common.action.IActionGroup;
+import org.apache.commons.collections4.CollectionUtils;
 import org.apache.commons.lang3.StringUtils;
 
 import javax.annotation.Nonnull;
 import javax.annotation.Nullable;
+import java.util.ArrayList;
 import java.util.Collection;
+import java.util.List;
 import java.util.Objects;
 import java.util.Optional;
 
@@ -30,11 +33,17 @@ public class ConnectionsNode extends AbstractTreeNode<AzureModule> implements IA
     @Nonnull
     public Collection<? extends AbstractTreeNode<?>> getChildren() {
         final AzureModule module = Objects.requireNonNull(this.getValue());
-        return Optional.of(module).stream()
+        final List<ConnectionNode> children = Optional.of(module).stream()
             .map(AzureModule::getDefaultProfile).filter(Objects::nonNull)
             .flatMap(p -> p.getConnections().stream())
             .map(r -> new ConnectionNode(module.getProject(), module, r))
             .toList();
+        if (CollectionUtils.isNotEmpty(children)) {
+            return children;
+        }
+        final ArrayList<AbstractTreeNode<?>> nodes = new ArrayList<>();
+        nodes.add(new ActionNode<>(module.getProject(), ResourceConnectionActionsContributor.CONNECT_TO_MODULE, module));
+        return nodes;
     }
 
     @Override
